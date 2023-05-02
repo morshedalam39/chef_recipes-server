0eb832cb74e1bcf747eb1f314a7855c8342d24f2
morshedalam39 <morshedalam4239@gmail.com>
2023-05-02 22:52:49 +0600

    cros added

--- index.js
+++ index.js
@@ -20,7 +20,21 @@ app.get('/details', (req, res) => {
     res.send(details);
 })
 
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedDetails = chef.find(d => d.id === id);
+    res.send(selectedDetails)
+})
 
+app.get('/details/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(details)
+    }
+    else {
+        const allDetails = details.filter(d => parseInt(d.chef_id) === id);
+        res.send(allDetails)
+    }
 
 })
 
